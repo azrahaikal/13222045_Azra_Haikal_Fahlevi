617ba97093fc2e4dd92c5792bb05c9f8898d003e
haikal <azrahaikal75@gmail.com>
2026-03-04 18:39:08 +0700

    upload latihan praktikum 4

---
+++ praktikum_4/latihan3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+int main() {  
+    std::vector<int> data = {10, 20, 30, 40};   
+    //int total = 0; // <-- TARGET BENTROKAN (DATA RACE)
+
+    // Simulasi: Jika 4 Core berebut mengubah variabel 'total' bersamaan  
+    #pragma omp parallel for
+    for(int i = 0; i < data.size(); i++) {  
+        int total = 0;
+        total = total + data[i];   
+    }
+
+    std::cout << "Total Akumulasi: " << total << std::endl;  
+    return 0;  
+}
